dd7b89723311a95f0c9ba2e9a8b3ebe596c5bbcf
Derek Garnett <drkgrntt@gmail.com>
2024-10-07 21:49:26 -0500

    Add email form

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"html/template"
 	"log"
@@ -10,6 +11,7 @@ import (
 	"time"
 
 	"github.com/joho/godotenv"
+	"github.com/mailgun/mailgun-go/v4"
 )
 
 type PageData struct {
@@ -18,6 +20,15 @@ type PageData struct {
 	Year    string
 	IsMedia bool
 	IsHomes bool
+	Page    string
+}
+
+type ContactInfo struct {
+	Name     string `form:"name"`
+	Email    string `form:"email"`
+	Phone    string `form:"phone"`
+	Message  string `form:"message"`
+	Honeypot string `form:"honeypot"`
 }
 
 func main() {
@@ -30,11 +41,65 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	http.HandleFunc("/homes", func(w http.ResponseWriter, r *http.Request) {
-		renderPage(w, "views/homes.html", PageData{IsHomes: true, Title: "Eric Hintermaier | Realtor"})
+		renderPage(w, "views/homes.html", PageData{Page: "homes", IsHomes: true, Title: "Eric Hintermaier | Realtor"})
 	})
 
 	http.HandleFunc("/media", func(w http.ResponseWriter, r *http.Request) {
-		renderPage(w, "views/media.html", PageData{IsMedia: true, Title: "HAIER | Creator"})
+		renderPage(w, "views/media.html", PageData{Page: "media", IsMedia: true, Title: "HAIER | Creator"})
+	})
+
+	http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
+		// Parse the form
+		err := r.ParseForm()
+		if err != nil {
+			fmt.Fprintf(w, "Error parsing form")
+			return
+		}
+
+		// Create an instance of the struct and fill it with form values
+		data := ContactInfo{
+			Name:     r.FormValue("name"),
+			Email:    r.FormValue("email"),
+			Phone:    r.FormValue("phone"),
+			Message:  r.FormValue("message"),
+			Honeypot: r.FormValue("honeypot"),
+		}
+
+		if data.Honeypot != "" {
+			fmt.Fprintf(w, "Error parsing form")
+			return
+		}
+
+		mg := mailgun.NewMailgun(os.Getenv("MG_DOMAIN"), os.Getenv("MG_API_KEY"))
+		sender := "Contact Form <noreply@haiertherealtor.com>"
+		subject := "New Contact!"
+		emailBody := fmt.Sprintf(
+			"New Message from %s\n%s\n%s\n\n%s",
+			data.Name,
+			data.Email,
+			data.Phone,
+			data.Message,
+		)
+		recipient := os.Getenv("RECIPIENT_EMAIL")
+
+		// The message object allows you to add attachments and Bcc recipients
+		message := mg.NewMessage(sender, subject, emailBody, recipient)
+
+		mgCtx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		// Send the message with a 10 second timeout
+		resp, id, err := mg.Send(mgCtx, message)
+
+		if err != nil {
+			fmt.Fprintf(w, "Error sending email")
+			return
+		}
+
+		fmt.Printf("ID: %s Resp: %s\n", id, resp)
+
+		fmt.Fprintf(w, "Thank you for reaching out! I will get back to you soon.")
+		return
 	})
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil))
@@ -44,6 +109,7 @@ func renderPage(w http.ResponseWriter, tmpl string, data PageData) {
 	renderTemplate(w, "views/layout.html", PageData{
 		IsHomes: data.IsHomes,
 		IsMedia: data.IsMedia,
+		Page:    data.Page,
 		Title:   data.Title,
 		Content: renderTemplateToString(tmpl, data),
 		Year:    fmt.Sprint(time.Now().Year()),
